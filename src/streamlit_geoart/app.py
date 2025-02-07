6185ba85d2c2076af06b4adad537b55043ca83e2
develmusa <contact@develmusa.digital>
2025-02-07 16:55:56 +0100

    simplify

--- src/streamlit_geoart/app.py
+++ src/streamlit_geoart/app.py
@@ -5,8 +5,9 @@ import streamlit as st
 import datetime
 import pendulum
 import matplotlib as mpl
-from streamlit_geoart.processor import ProcessData, generate_year_temp_art
+from streamlit_geoart.processor import ProcessData, generate_year_temp_art 
 from pydantic import BaseModel
+
 
 class session_state_user_data(BaseModel):
     process_data: ProcessData = None
@@ -28,58 +29,25 @@ st.write("""
     """)
 
 if not st.session_state:
-    st.session_state.process_data = None
-
-# with open("./src/streamlit-geoart/examples.json", "r", encoding="utf8") as f:
-    # EXAMPLES = json.load(f)
-# st.session_state.update(EXAMPLES["example"])
-
-if not st.session_state.process_data:
     st.session_state.update(session_state_user_data())
-
-# for item in st.session_state.items():
-    # st.write(item)
-
-
 
 
 form_col1, form_col2= st.columns(2)
 address = form_col1.text_input(label="Location", key="location")
 start_date = form_col2.date_input("Start Date", min_value=datetime.date(1940, 1, 1), max_value=datetime.datetime.now()- datetime.timedelta(days=366), key="start_date")
 
+progress = st.empty()
 
-with st.status("Creating Arte...", expanded=True) as status:
-    def status_progress_callback(message: str):
-        status.update(label=message, state="running", expanded=True)
 
-    def status_error_callback(message: str):
-        status.update(label=message, state="error")
-
-    def status_success_callback(message: str, data: ProcessData):
-        status.update(label=message, state="complete", expanded=True)
-        st.session_state.process_data = data
-    
-    generate_year_temp_art(
-        location_address=st.session_state.location,
-        color_map=st.session_state.style,
-        start_date=st.session_state.start_date,
-        success_callback=status_success_callback,
-        progress_callback=status_progress_callback,
-        error_callback=status_error_callback
-    )
 def generate_data():
     ...
-    generate_year_temp_art(
+    data = generate_year_temp_art(
         location_address=st.session_state.location,
         color_map=st.session_state.style,
         start_date=st.session_state.start_date,
-        success_callback=status_success_callback,
-        progress_callback=status_progress_callback,
-        error_callback=status_error_callback
     )
-
-
-data: ProcessData = st.session_state.process_data
+    st.session_state.process_data = data
+generate_data()
 
         
 process_data: ProcessData = st.session_state.process_data
@@ -91,8 +59,9 @@ option = st.selectbox(
     on_change=generate_data,
 )
 
+
 st.image(process_data.image.get_image())
-# print(process_data.model_dump_json)
+
 col1, col2, col3 = st.columns(3)
     # Convert hourly weather data to a DataFrame
 df = process_data.weather_data.hourly.to_dataframe()
@@ -118,5 +87,5 @@ with col3:
     mean_temp_str = f"{df['temperature'].mean().round()} °C"
     st.metric(label="Mean", label_visibility="hidden", value=mean_temp_str)
 
-# map_data = pd.DataFrame([st.session_state.process_data.location_coordinates.model_dump()])
-# st.map(data=map_data, zoom=10, use_container_width=True)
+    # map_data = pd.DataFrame([st.session_state.process_data.location_coordinates.model_dump()])
+    # st.map(data=map_data, zoom=10, use_container_width=True)
--- src/streamlit_geoart/processor.py
+++ src/streamlit_geoart/processor.py
@@ -15,6 +15,8 @@ from geoart.weather_data import WeatherData
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
+import streamlit as st
+
 class ProcessData(BaseModel):
     location_address: str
     location_coordinates: Optional[Coordinates] = None
@@ -24,62 +26,36 @@ class ProcessData(BaseModel):
     image: Optional[Image] = None
     # image: Optional[np.ndarray] = None  # To hold serialized NumPy array    image_array: Optional[np.ndarray] = None
 
-class Notifier:
-    def __init__(self, success_callback: Callable[[str, ProcessData], None], progress_callback: Optional[Callable[[str], None]] = None, error_callback: Optional[Callable[[str], None]] = None):
-        self.success_callback = success_callback
-        self.progress_callback = progress_callback
-        self.error_callback = error_callback
-
-    def notify_progress(self, message: str):
-        if self.progress_callback:
-            self.progress_callback(message)
-
-    def notify_error(self, error_message: str):
-        if self.error_callback:
-            self.error_callback(error_message)
-
-    def notify_success(self, message: str, process_data: ProcessData):
-        self.success_callback(message, process_data)
-
-
-
-
-
+@st.cache_data
 def generate_year_temp_art(
     location_address: str,
     color_map: str,
     start_date: datetime.date,
-    success_callback: Optional[Callable[[str, ProcessData], None]] = None,
-    progress_callback: Optional[Callable[[str], None]] = None,
-    error_callback: Optional[Callable[[str], None]] = None,
-) -> None:  # Define the exact return type if possible
-    
-    notifier = Notifier(success_callback=success_callback, progress_callback=progress_callback, error_callback=error_callback)
+) -> ProcessData:  # Define the exact return type if possible
     process_data = ProcessData(location_address=location_address, start_date=start_date)
     process_data.end_date = start_date.replace(year=start_date.year + 1)
-    # process_data.end_date = start_date.replace(year=start_date.year + 1).date()
 
-    async def async_wrapper():
-
+    async def async_wrapper() -> ProcessData:
         try:
-            notifier.notify_progress("Fetching coordinates...")
             process_data.location_coordinates = geolocation.address_to_coordinates(address=location_address)
-            notifier.notify_progress("Fetching weather data...")
-            process_data.weather_data = await weather_data.fetch_weather_data(location_point=process_data.location_coordinates, start_date=process_data.start_date, end_date=process_data.end_date)
-            # process_data.image = create_image(process_data.weather_data.hourly.to_dataframe_byte_normalized())
+            process_data.weather_data = await weather_data.fetch_weather_data(
+                location_point=process_data.location_coordinates,
+                start_date=process_data.start_date,
+                end_date=process_data.end_date
+            )
             process_data.image = create_image(process_data.weather_data.hourly.to_dataframe(), color_map)
-            
-            notifier.notify_success("Creating Art Complete", process_data)
-
-
-            
+            return process_data
 
         except geolocation.AddressNotFoundError:
-            notifier.notify_error("Address not found")
+            st.error(f"Could not find coordinates for address: {location_address}")
+            return process_data
 
         except weather_data.WeatherDataError:
-            notifier.notify_error("Error fetching weather data")
+            st.error(f"Could not fetch weather data for {location_address}")
+            return process_data
 
         except Exception as e:
-            notifier.notify_error(f"Unexpected Error {e}")
-    asyncio.run(async_wrapper())
+            st.error(f"An unexpected error occurred: {str(e)}")
+            return process_data
+
+    return asyncio.run(async_wrapper())
